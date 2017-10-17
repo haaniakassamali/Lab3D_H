dcf08adf769bad3da573cfa32a600cbdab6269a9
guardul0036 <32370189+guardul0036@users.noreply.github.com>
2017-10-17 17:05:09 -0500

    Update work.py

--- work.py
+++ work.py
@@ -20,24 +20,9 @@ def requests_per_month():
     data = urllib.request.urlopen('https://s3.amazonaws.com/tcmg412-fall2016/http_access_log')
     
     for line in data:
-        while count < 5:
+        #displays each line in the file
             x = str(line)
-            #creating a helpful list with date, get, number to work with
             
-            a = x.split('[')
-            b = a[1]
-            c = b.split(']')
-            d = [a[0], c[0], c[1]]
-            aa = d[2]
-            bb = aa.split('"')
-            aaa = bb[2]
-            ccc = aaa.split()
-            totes = [a[0], c[0], bb[1], ccc[0],ccc[1]]
-            
-            
-            
-            count += 1
-  
 def main():
     requests_per_month()
     
