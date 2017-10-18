158344b5a5f7e2770b09bf90d85a5db22b8b581a
guardul0036 <32370189+guardul0036@users.noreply.github.com>
2017-10-18 12:57:28 -0500

    Update work.py
    Requests per month and total requests finally works!
    Still need to get the other functions to work 
    Next function to work on: percentage of requests that were not successful 
    working functions: 2

--- work.py
+++ work.py
@@ -1,8 +1,8 @@
 import urllib.request
 from datetime import datetime
 
-def requests_per_month_and_month_files():
-    count = 0
+def requests_per_month():
+    Total_count = 0
     
     Oct = 0
     Nov = 0
@@ -21,67 +21,107 @@ def requests_per_month_and_month_files():
     data = urllib.request.urlopen('https://s3.amazonaws.com/tcmg412-fall2016/http_access_log')
     
     for line in data:
-        while count < 5:
-            #Turning each line into strings
-            x = str(line)
-            #ignore the weird lines #couldn't figure out how to work with them
-            if len(x) < 50: pass
+        #Turning each line into strings
+        x = str(line)
+        #ignore the weird lines #couldn't figure out how to work with them
+        if len(x) < 74: pass
             
-            else:    
-                #creating a helpful list that seperates each line into sections
-                a = x.split('[')
-                b = a[1]
-                c = b.split(']')
-                d = [a[0], c[0], c[1]]
-                aa = d[2]
-                bb = aa.split('"')
+        else:    
+            #creating a helpful list that seperates each line into sections
+            a = x.split('[')
+            b = a[1]
+            c = b.split(']')
+            d = [a[0], c[0], c[1]]
+            aa = d[2]
+            bb = aa.split('"')
+            
+            #having problems in this area due to length of some lines 
+            
+            if len(bb) < 3:
+                totes = [a[0], c[0], bb[1]]
+            else:
                 aaa = bb[2]
                 ccc = aaa.split()
-                totes = [a[0], c[0], bb[1], ccc[0],ccc[1]]
+                if len(ccc) < 2:
+                    totes = [a[0], bb[1], ccc[0]]
+                else:
+                    totes = [a[0], c[0], bb[1], ccc[0],ccc[1]]
                 
-                #counting requests for each month 
+            #counting requests for each month 
             
-                if 'Oct' in totes[1]:
-                    Oct += 1
-                if 'Nov' in totes[1]:
-                    Nov += 1 
-                if 'Dec' in totes[1]:
-                    Dec += 1
-                if 'Jan' in totes[1]:
-                    Jan += 1
-                if 'Feb' in totes[1]:
-                    Feb += 1
-                if 'Mar' in totes[1]:
-                    Mar += 1
-                if 'Apr' in totes[1]:
-                    Apr += 1
-                if 'May'in totes[1]:
-                    May += 1
-                if 'Jun' in totes[1]:
-                    Jun += 1
-                if 'Jul' in totes[1]:
-                    Jul += 1
-                    
-                print('Total requests made for October:  ...', Oct)
-                print('Total requests made for November:   ', Nov)
-                print('Total requests made for December: ...', Dec)
-                print('Total requests made for January:     ', Jan)
-                print('Total requests made for February: ...', Feb)
-                print('Total requests made for March:       ', Mar)
-                print('Total requests made for April:    ...', Apr)
-                print('Total requests made for May:         ', May)
-                print('Total requests made for June:     ...', Jun)
-                print('Total requests made for July:        ', Jul)
+            if 'Oct' in totes[1]:
+                Oct += 1
+                Total_count += 1
                 
-                count += 1
+            elif 'Nov' in totes[1]:
+                Nov += 1 
+                Total_count += 1
+                
+            elif 'Dec' in totes[1]:
+                Dec += 1
+                Total_count += 1
+                
+            elif 'Jan' in totes[1]:
+                Jan += 1
+                Total_count += 1
+                
+            elif 'Feb' in totes[1]:
+                Feb += 1
+                Total_count += 1
+                
+            elif 'Mar' in totes[1]:
+                Mar += 1
+                Total_count += 1
+                
+            elif 'Apr' in totes[1]:
+                Apr += 1
+                Total_count += 1
+                
+            elif 'May'in totes[1]:
+                May += 1
+                Total_count += 1
+                
+            elif 'Jun' in totes[1]:
+                Jun += 1
+                Total_count += 1
+                
+            else:
+                Jul += 1
+                Total_count += 1
+                    
+    print('Total requests made for October:                 ...', Oct)
+    print('Total requests made for November:                   ', Nov)
+    print('Total requests made for December:                ...', Dec)
+    print('Total requests made for January:                    ', Jan)
+    print('Total requests made for February:                ...', Feb)
+    print('Total requests made for March:                      ', Mar)
+    print('Total requests made for April:                   ...', Apr)
+    print('Total requests made for May:                        ', May)
+    print('Total requests made for June:                    ...', Jun)
+    print('Total requests made for July:                       ', Jul)
+                
 
 def total_requests():
-    #still in progress 
-    total = 0
-    print('\n The total requests made during this time period: ')
+    
+    Total_count = 0
+    
+    data = urllib.request.urlopen('https://s3.amazonaws.com/tcmg412-fall2016/http_access_log')
+    
+    for line in data:
+        
+        x = str(line)
+        if len(x) < 74: pass
+            
+        else:    
+            Total_count += 1
+                       
+    
+    total = Total_count
+    print('The TOTAL requests made during this time period: ...', total,'\n')
     
 def main():
     print('\n-----------------------------------------\n')
-    requests_per_month_and_month_files()
+    total_requests()
+    requests_per_month()
     
 main()
